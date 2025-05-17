76ba47a1d9a9167131371c89575bbd0a80ac2fe0
johannalsyb <79452051+johannalsyb@users.noreply.github.com>
2025-05-17 20:16:34 +0100

    Update index.js

--- Kazaswap/src/index.js
+++ Kazaswap/src/index.js
@@ -1,16 +1,10 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+import * as Sentry from '@sentry/react-native';
 
 /* Firebase */
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Firebase config...
 const firebaseConfig = {
   apiKey: "AIzaSyDW9P8xHeJiqJk-K3AVyzH6gwz4H1WdJbo",
   authDomain: "kazaswap.firebaseapp.com",
@@ -21,13 +15,13 @@ const firebaseConfig = {
   measurementId: "G-JJZ8RS3NMP"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-/* ================== */
+
+/* ✅ TEST SENTRY ERROR */
+Sentry.captureException(new Error("💥 Test error from Kazaswap"));
 
 AppRegistry.registerComponent("App", () => App);
 AppRegistry.runApplication("App", {
-    rootTag: document.getElementById("react-root")
+  rootTag: document.getElementById("react-root")
 });
-  

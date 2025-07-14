0fc798e1d2cff5c018d2c12c5638a219b586ec8e
naveedbasharat06 <naveed.basharat06@gmail.com>
2025-07-15 04:50:32 +0500

    sending email using dynamic sendgrid template

--- api/src/services/emails/sendgrid.ts
+++ api/src/services/emails/sendgrid.ts
@@ -2,27 +2,37 @@ import { SENDGRID_APIKEY } from "../../config"
 import { request } from "../../utils"
 import { EmailParams, defaultFrom } from "../email"
 
-export default async (params: EmailParams) => request("https://api.sendgrid.com/v3/mail/send",{
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${SENDGRID_APIKEY}`
-    },
-    body: JSON.stringify({
-        personalizations: [{
-            to: params.to,
-            cc: params.cc,
-            bcc: params.bcc
-        }],
-        from: params.from || defaultFrom,
+export default async (params: EmailParams) => {
+  const body: any = {
+    personalizations: [
+      {
         to: params.to,
         cc: params.cc,
         bcc: params.bcc,
-        subject: params.subject,
-        content: [{
-            type: params.contentType || "text/plain",
-            value: params.content
-        }],
-    })
-})
-.then(r => r.json())
+        dynamic_template_data: params.dynamic_template_data
+      }
+    ],
+    from: params.from || defaultFrom,
+  };
+
+  if (params.template_id) {
+    body.template_id = params.template_id;
+  } else {
+    body.subject = params.subject;
+    body.content = [
+      {
+        type: params.contentType || "text/plain",
+        value: params.content,
+      },
+    ];
+  }
+
+  return request("https://api.sendgrid.com/v3/mail/send", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${SENDGRID_APIKEY}`
+    },
+    body: JSON.stringify(body)
+  }).then((r) => r.json());
+};
